ae1d93c08e0c26bc37fce15cf0fc14e8a1e73321
FeldherrWeiprecht <Dragi2008#>
2024-05-10 15:13:45 +0200

    Den Antwort-Buttons ein ClickEvent gegeben sowie dort drinnen die States der Buttons gesetzt

--- MillionenShowApp/MainWindow.xaml.cs
+++ MillionenShowApp/MainWindow.xaml.cs
@@ -21,7 +21,73 @@ namespace MillionenShowApp
 			InitializeComponent();
 		}
 
+		private void disableAnswerButtons()
+		{
+			answerButtonA.IsEnabled = false;
+			answerButtonB.IsEnabled = false;
+			answerButtonC.IsEnabled = false;
+			answerButtonD.IsEnabled = false;
+		}
 
+		private void enableAnswerButtons()
+		{
+			answerButtonA.IsEnabled = true;
+			answerButtonB.IsEnabled = true;
+			answerButtonC.IsEnabled = true;
+			answerButtonD.IsEnabled = true;
+		}
 
+		private async void answerButtonA_Click(object sender, RoutedEventArgs e)
+		{
+			disableAnswerButtons();
+
+			answerButtonA.Background = Brushes.Green;
+
+			await Task.Delay(TimeSpan.FromSeconds(3));
+
+			answerButtonA.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+
+			enableAnswerButtons();
+
+		}
+
+		private async void answerButtonB_Click(object sender, RoutedEventArgs e)
+		{
+			disableAnswerButtons();
+
+			answerButtonB.Background = Brushes.Green;
+
+			await Task.Delay(TimeSpan.FromSeconds(3));
+
+			answerButtonB.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+
+			enableAnswerButtons();
+		}
+
+		private async void answerButtonC_Click(object sender, RoutedEventArgs e)
+		{
+			disableAnswerButtons();
+
+			answerButtonC.Background = Brushes.Green;
+
+			await Task.Delay(TimeSpan.FromSeconds(3));
+
+			answerButtonC.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+
+			enableAnswerButtons();
+		}
+
+		private async void answerButtonD_Click(object sender, RoutedEventArgs e)
+		{
+			disableAnswerButtons();
+
+			answerButtonD.Background = Brushes.Green;
+
+			await Task.Delay(TimeSpan.FromSeconds(3));
+
+			answerButtonD.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+
+			enableAnswerButtons();
+		}
 	}
 }
